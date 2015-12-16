70eb64c9c3800cd35faa88f540e6110b6704a93e
Stephen Beecher <sjbeecher@gmail.com>
2015-12-16 15:05:42 +0000

    a test program for temp conv

---
+++ test_temp_conversion.py
@@ -0,0 +1,11 @@
+from temp_conversion import fahr_to_celsius
+from temp_conversion import fahr_to_kelvin
+
+def cel_test_null():
+    fahr_to_celsius(0)==(-32/9)*5
+def kel_test_null():
+    fahr_to_kelvin(0)==(-32*(5/9))-273.15
+def cel_test_zero():
+    fahr_to_celsius(32)==0
+def kel_test_zero():
+    fahr_to_kelvin(-32)==-273.15
